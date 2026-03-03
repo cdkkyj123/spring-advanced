4d631da822b49f08f4d76dd55f321117ef616faa
cdkkyj123 <cdkkyj123@gmail.com>
2026-03-03 11:40:30 +0900

    1-1 Run Success

--- src/main/java/org/example/expert/config/JwtUtil.java
+++ src/main/java/org/example/expert/config/JwtUtil.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Date;
 
@@ -24,14 +24,14 @@ public class JwtUtil {
     private static final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
 
     @Value("${jwt.secret.key}")
-    private String secretKey;
-    private Key key;
+    private String secretKeyString;
+    private SecretKey key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
     @PostConstruct
     public void init() {
-        byte[] bytes = Base64.getDecoder().decode(secretKey);
-        key = Keys.hmacShaKeyFor(bytes);
+        byte[] bytes = Base64.getDecoder().decode(secretKeyString);
+        this.key = Keys.hmacShaKeyFor(bytes);
     }
 
     public String createToken(Long userId, String email, UserRole userRole) {
@@ -50,7 +50,7 @@ public class JwtUtil {
 
     public String substringToken(String tokenValue) {
         if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
-            return tokenValue.substring(7);
+            return tokenValue.substring(BEARER_PREFIX.length());
         }
         throw new ServerException("Not Found Token");
     }
